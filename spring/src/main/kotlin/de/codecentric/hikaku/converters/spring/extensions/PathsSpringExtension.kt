baff6a3ba4804315d82bc98415c79a629e9dfda1
Adam <897017+aSemy@users.noreply.github.com>
2022-04-28 18:43:58 +0200

    tidy up Spring paths extractor

--- spring/src/main/kotlin/de/codecentric/hikaku/converters/spring/extensions/PathsSpringExtension.kt
+++ spring/src/main/kotlin/de/codecentric/hikaku/converters/spring/extensions/PathsSpringExtension.kt
@@ -2,7 +2,7 @@ package de.codecentric.hikaku.converters.spring.extensions
 
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo
 
-internal fun RequestMappingInfo.paths(): Set<String> = buildSet {
-  patternsCondition?.patterns?.let(::addAll)
-  pathPatternsCondition?.patterns?.map { it.patternString }?.let(::addAll)
-}
+internal fun RequestMappingInfo.paths(): Set<String> =
+  patternsCondition?.patterns
+    ?: pathPatternsCondition?.patterns?.map { it.patternString }?.toSet()
+    ?: emptySet()

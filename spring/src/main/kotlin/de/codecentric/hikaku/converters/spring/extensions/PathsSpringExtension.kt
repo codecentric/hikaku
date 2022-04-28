ad2803bf8fcfad61d802ed75eb97d552506a2def
aSemy <897017+aSemy@users.noreply.github.com>
2022-04-28 18:37:23 +0200

    #113 Spring fix paths
    fetch paths from patternsCondition and pathPatternsCondition

--- spring/src/main/kotlin/de/codecentric/hikaku/converters/spring/extensions/PathsSpringExtension.kt
+++ spring/src/main/kotlin/de/codecentric/hikaku/converters/spring/extensions/PathsSpringExtension.kt
@@ -2,6 +2,7 @@ package de.codecentric.hikaku.converters.spring.extensions
 
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo
 
-internal fun RequestMappingInfo.paths(): Set<String> {
-    return this.patternsCondition?.patterns ?: emptySet()
+internal fun RequestMappingInfo.paths(): Set<String> = buildSet {
+  patternsCondition?.patterns?.let(::addAll)
+  pathPatternsCondition?.patterns?.map { it.patternString }?.let(::addAll)
 }
